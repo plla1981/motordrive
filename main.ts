8bbc8ddcf581071fea224ac5c9e6f38001d0e936
Plla <1183136570@qq.com>
2022-10-18 10:59:13 +0800

    更新 pxt.json, main.blocks, main.ts, motordrive.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
 basic.forever(function () {
-    Motor.motor(Motorlist.M1, Direction1.Backward, 200)
-    Motor.motor(Motorlist.M2, Direction1.Forward, 200)
+    Motor.motor(Motorlist.M1, Direction1.Backward, 255)
+    Motor.motor(Motorlist.M2, Direction1.Forward, 255)
+    Motor.led_rgb(
+    LED_rgb_L_R.LED_R,
+    0,
+    0,
+    0
+    )
+    basic.pause(1000)
+    Motor.led_rgb(
+    LED_rgb_L_R.LED_L,
+    0,
+    0,
+    0
+    )
+    Motor.motor(Motorlist.M1, Direction1.Forward, 255)
+    Motor.motor(Motorlist.M2, Direction1.Backward, 255)
+    basic.pause(1000)
 })
--- motordrive.ts
+++ motordrive.ts
@@ -66,6 +66,13 @@ enum Direction1
     Backward = 0
 }
 
+enum LED_rgb_L_R {
+    //% bolck="LED_R"
+    LED_R = 1,
+    //% bolck="LED_L"
+    LED_L = 0,
+}
+
 //% color="#AA278D"
 namespace Motor {
 
@@ -85,6 +92,19 @@ namespace Motor {
                 else { motor_i2cWrite(0x04, 255-pwmvalue); motor_i2cWrite(0x03, 255); }
                 break;
         }
+    }
+    
+    //% block="RGB = %place rad   = $arg1 green =$arg2 blue  = $arg3"
+    //% direction.shadow=timePicker
+    //% pwmvalue.min=0 pwmvalue.max=1 
+    export function led_rgb(place: LED_rgb_L_R, arg1: number, arg2: number, arg3: number) {
+        switch (place) {
+            case 0: { motor_i2cWrite(0x09, arg1); motor_i2cWrite(0x0a, arg2); motor_i2cWrite(0x0b, arg3); }
+                break;
+            case 1: { motor_i2cWrite(0x0c, arg1); motor_i2cWrite(0x0d, arg2); motor_i2cWrite(0x0e, arg3); }
+                break;
+        }
+
     }
 
     function motor_i2cWrite(reg: number, value: number) 
