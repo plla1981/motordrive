b538bb3f12bbcd3df98fcfe27d0fe67879799f4f
Plla <1183136570@qq.com>
2023-01-10 09:08:03 +0800

    更新 main.blocks, main.ts, motordrive.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    Motor.motor(Motorlist.M1, Direction1.Backward, 255)
+    Motor.motor(Motorlist.M1, Direction1.Forward, 255)
     Motor.motor(Motorlist.M2, Direction1.Forward, 255)
     Motor.led_rgb(
     LED_rgb_L_R.LED_R,
@@ -15,6 +15,6 @@ basic.forever(function () {
     0
     )
     Motor.motor(Motorlist.M1, Direction1.Forward, 255)
-    Motor.motor(Motorlist.M2, Direction1.Backward, 255)
+    Motor.motor(Motorlist.M2, Direction1.Forward, 255)
     basic.pause(1000)
 })
